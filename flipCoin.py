a7ea42cf721b9458853d72f332c60d90148128b5
Sudha Sahu <sudhasahu52@gmail.com>
2022-03-21 13:02:17 +0530

    sudha-refactored the code

--- flipCoin.py
+++ flipCoin.py
@@ -2,21 +2,27 @@
 
 import random
 
-no_of_flip = int(input("Enter the number of times to flip the coin : "))
-total_heads = 0
-total_tails = 0
-cnt = 0
-while cnt < no_of_flip:
-    coin = random.randint(0, 1)
-    if coin < 0.5:
-        total_tails += 1
-    else:
-        total_heads += 1
-    cnt += 1
-print("Heads came up", total_heads, "times")
-print("Tails came up", total_tails, "times")
 
-calc_per_head = (total_heads/no_of_flip)*100
-print("Percentage of Head is : ", calc_per_head, "%")
-calc_per_tail = (total_tails/no_of_flip)*100
-print("Percentage of Tail is : ", calc_per_tail, "%")
+def count_head_tail(no_of_flip):
+    total_heads = 0
+    total_tails = 0
+    cnt = 0
+    while cnt < no_of_flip:
+        coin = random.randint(0, 1)
+        if coin < 0.5:
+            total_tails += 1
+        else:
+            total_heads += 1
+        cnt += 1
+    print("Heads came up", total_heads, "times")
+    print("Tails came up", total_tails, "times")
+
+    calc_per_head = (total_heads/no_of_flip)*100
+    print("Percentage of Head is : ", calc_per_head, "%")
+    calc_per_tail = (total_tails/no_of_flip)*100
+    print("Percentage of Tail is : ", calc_per_tail, "%")
+
+
+no_of_times = int(input("Enter the number of times to flip the coin : "))
+count_head_tail(no_of_times)
+
